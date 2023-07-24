5f5f4909b520b5f7b30fac9459e924a79172bdfa
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-07-24 18:48:45 +0400

    feat: added route

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 import { createTable } from "./config/sql.js";
-import express from express;
+import express from "express";
 
-app.express = express();
+const app = express();
 
-async function init(){
-    try {
-        await createTable();
-        serverStart()
-    } catch (error) {
-        console.log(error);
-        
-    }
-    function serverStart(){
-        app.listen(3000)
-    }
-
+async function init() {
+  try {
+    await createTable();
+    serverStart();
+  } catch (error) {
+    console.log(error);
+  }
+  function serverStart() {
+    app.get("/", (req, res) => {
+      return res.status(200).json({ message: "works!" });
+    });
+    app.listen(3000);
+  }
 }
 
-init()
+init();
